c8c47a5c09521bb1a205c4af7c22448ace870479
Vlad <151467742+fgolik@users.noreply.github.com>
2023-11-23 22:26:41 +0300

    Добавьте файлы проекта.

---
+++ 2.7/2.7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>//variant 3
+#include <math.h>//Заданы натуральные числа m, n. Вводятся целые числа а1, ..., an. Вычислить b1 + ... + bm, где b1 = а1 + а2 + ... + an; b2 = а1 2 + а2 2 + ... + an 2 ; ... bm = а1 m + а2  m + ... + an m
+using namespace std;
+
+int main()
+{
+	int b_len;
+	cout << " Enter m = ";
+	cin >> b_len;
+	int a_len;
+	cout << "Enter n = ";
+	cin >> a_len;
+
+	int sum_el_b = 0;
+	int el_a;
+
+	for (int i = 0; i < a_len; ++i)
+	{
+		cout << "Enter quantity element a =  "<< endl;
+		cin >> el_a;
+
+		for (int n = 1; n <= b_len; ++n)
+		{
+			sum_el_b += pow(el_a, n);
+		}
+	}
+
+	cout << sum_el_b << endl;
+}
